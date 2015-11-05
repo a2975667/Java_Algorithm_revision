6929d4b98b8b58f2fa40332450477fbbd92f16a0
tcheng <tcheng@link.cuhk.edu.hk>
2015-11-05 18:22:50 +0800

    20151105 1822 update linked list

--- Java code/src/main.java
+++ Java code/src/main.java
@@ -1,9 +1,11 @@
+import Ds_LinkedList.Ds_LinkedList;
+
 /**
  * Created by tcheng on 2015/11/3.
  */
 public class main {
     public static void main(String[] args){
-        Ds_Queue.run();
+        Ds_LinkedList.run();
 
     }
 }
